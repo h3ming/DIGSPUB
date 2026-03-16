3ea2a2d744f8e625b3563ed2b7973a8811bc5987
h3ming <62722344+h3ming@users.noreply.github.com>
2026-03-16 15:59:03 -0500

    oops

--- my-site/src/routes/playground/+page.svelte
+++ my-site/src/routes/playground/+page.svelte
@@ -79,7 +79,7 @@
 
 <!-- Get your own Fortune Input and Button  -->
 <div class="flex justify-center gap-2 mt-6">
-  <Input aria-label="Ask the ball" bind:value={question} placeholder="What's my fortune..." onclick={handleEnter}class="w-64" />
+  <Input aria-label="Ask the ball" bind:value={question} placeholder="What's my fortune..." onkeydown={handleEnter} class="w-64" />
   <Button onclick={ask8ball}>Ask!</Button>
 </div>
 
